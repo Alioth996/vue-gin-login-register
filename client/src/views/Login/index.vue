9676b59a2a0ceca4bac0189834bdc208f5f7b65c
alex <2844520415@qq.com>
2022-09-01 03:35:06 +0800

    add bug desc

--- client/src/views/Login/index.vue
+++ client/src/views/Login/index.vue
@@ -41,9 +41,11 @@ const loginUser = async (v) => {
     timer = setTimeout(() => {
         router.replace({
             name: 'home',
+            // query正常
             query: {
                 username: v.username
             },
+            // params: 似乎必须使用动态路由才能传参,不然则为空,知识盲区
             params: {
                 username: v.username
             }
@@ -65,6 +67,7 @@ const registerUser = async (v) => {
             query: {
                 username: v.username
             },
+
             params: {
                 username: v.username
             }
