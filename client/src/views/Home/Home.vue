6c2e6e3e5fb934d05cc560996d7c37d88d012423
alex <2844520415@qq.com>
2022-09-03 19:37:18 +0800

    feat: add home page

--- client/src/views/Home/Home.vue
+++ client/src/views/Home/Home.vue
@@ -1,60 +1,38 @@
 <template>
     <div id="home">
-        <fieldset>
-            <legend>用户信息</legend>
-            <p>欢迎VIP:
-                <span style="color: #f10;font-size: 20px;font-weight: 600;">{{ route.query.username }}</span>
-                登录云熬夜系统,祝您辞藻药丸~~~(doge)
-            </p>
-
-        </fieldset>
-        <fieldset>
-            <legend>系统操作</legend>
-            <p>
-                <el-button @click="logout">下线</el-button>
-                <el-button @click="delAccount">注销</el-button>
-            </p>
-        </fieldset>
+        <el-container>
+            <el-aside width="220px">
+                <home-menu />
+            </el-aside>
+            <el-container>
+                <el-header>
+                    <home-header />
+                </el-header>
+                <el-main>
+                    <router-view />
+                </el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
 <script setup>
-import { useRoute, useRouter } from 'vue-router';
-
-const route = useRoute()
-const router = useRouter()
-
-// 下线
-const logout = () => {
-    localStorage.removeItem('token')
-    router.replace('/login')
-}
-const delAccount = () => {
-    // delete请求后端删除当前用户
-    
-
-    localStorage.removeItem('token')
-    router.replace('/login')
-}
-
+import HomeHeader from "./components/HomeHeader.vue";
+import HomeMenu from "./components/HomeMenu.vue";
 </script>
 
 <style scoped lang='less'>
 #home {
     width: 100%;
     height: 100%;
-    display: flex;
-    gap: 20px;
-    flex-direction: column;
-    align-items: center;
-    background: url('../../assets/images/background.5825f033.svg') no-repeat center center/cover;
-}
 
-fieldset {
-    &:first-child {
-        margin-top: 200px;
+    .el-container {
+        width: 100%;
+        height: 100%;
     }
 
-    width: 700px;
+    .el-header {
+        padding: 0;
+    }
 }
 </style>
